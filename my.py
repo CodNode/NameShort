ae5f169f64b02c884d5f2f0fbe3d97d28d7978bd
Muhammad Hussain <samsteve124@gmail.com>
2017-09-27 00:37:59 +0530

    Added 7 line version of you madhav

---
+++ my.py
@@ -0,0 +1,10 @@
+#  Don't mess with me ;)
+
+name = input("Enter your name ")
+cap = name.title()
+arr = cap.split(" ")
+first = arr[0]
+second = arr[1]
+third = arr[2]
+
+print (first[0:1] + ". " + second[0:1] + ". " + third)
