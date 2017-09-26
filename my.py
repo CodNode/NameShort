463a76bd13435b340a537b6e11dbefdaf5c0a148
Muhammad Hussain <samsteve124@gmail.com>
2017-09-27 00:41:33 +0530

    Update my.py

--- my.py
+++ my.py
@@ -3,8 +3,12 @@
 name = input("Enter your name ")
 cap = name.title()
 arr = cap.split(" ")
-first = arr[0]
-second = arr[1]
-third = arr[2]
-
-print (first[0:1] + ". " + second[0:1] + ". " + third)
+if len(arr)  == 2:
+    first = arr[0]
+    second = arr[1]
+    print (first[0:1] + ". " + second)
+elif len(arr) == 3:
+    first = arr[0]
+    second = arr[1]
+    third = arr[2]
+    print (first[0:1] + ". " + second[0:1] + ". " + third)
